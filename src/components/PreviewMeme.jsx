f45bc7ee20231006f6aa1497aa0074f2a2b4786e
AndresParraGO <soyandresgarzon@gmail.com>
2021-10-27 08:47:13 -0500

    Add funtionality to edit text more easily

--- src/components/PreviewMeme.jsx
+++ src/components/PreviewMeme.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Draggable from 'react-draggable';
+import Draggable from "react-draggable";
 import useEditor from "../hooks/useEditor";
 
 const WrapperPreviewMeme = styled.div`
@@ -13,12 +13,13 @@ const Meme = styled.div`
   width: 100%;
   max-width: 350px;
   overflow: hidden;
-  
-  p {
+  text-align: center;
+
+  input {
+    background-color: transparent;
     margin: 0;
-    position: absolute;
     cursor: pointer;
-    width: 100%;
+    width: 90%;
     font-family: ${(props) => props.optionsMeme.fontFamily};
     font-size: ${(props) => props.optionsMeme.fontSize}px;
     font-weight: bold;
@@ -26,19 +27,41 @@ const Meme = styled.div`
     color: #fff;
     text-shadow: 2px 0 0 #000, -2px 0 0 #000, 0 2px 0 #000, 0 -2px 0 #000,
       1px 1px #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000;
+    position: absolute;
+    left: 5%;
+  }
+
+  input:focus {
+    border: 2px solid ${(props) => props.theme.colors.primary};
   }
 `;
 
-const TextTop = styled.p`
+const TextTop = styled.input`
   top: 20px;
-`
+`;
 
-const TextBottom = styled.p`
+const TextBottom = styled.input`
   bottom: 20px;
-`
+`;
 
 function PreviewMeme() {
-  const { meme, textTop, textBottom, memeRef, optionsMeme } = useEditor();
+  const {
+    meme,
+    textTop,
+    textBottom,
+    setTextTop,
+    setTextBottom,
+    memeRef,
+    optionsMeme,
+  } = useEditor();
+
+  const handleChangeTextTop = (e) => {
+    setTextTop(e.target.value);
+  };
+
+  const handleChangeTextBottom = (e) => {
+    setTextBottom(e.target.value);
+  };
 
   return (
     <WrapperPreviewMeme>
@@ -46,11 +69,11 @@ function PreviewMeme() {
         <img src={meme} />
 
         <Draggable>
-          <TextTop>{textTop}</TextTop>
+          <TextTop value={textTop} onChange={handleChangeTextTop} />
         </Draggable>
 
         <Draggable>
-          <TextBottom>{textBottom}</TextBottom>
+          <TextBottom value={textBottom} onChange={handleChangeTextBottom} />
         </Draggable>
       </Meme>
     </WrapperPreviewMeme>
