0cfd381fc3939c60bb3f7f8551df0c846a113ce3
andresparradev <soyandresgarzon@gmail.com>
2021-11-07 10:45:47 -0500

    Config env in vitejs

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 const API_MEMES_URL = "https://api.imgflip.com";
-const API_BASE_URL = "http://localhost:9000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE;
 
 export { API_MEMES_URL, API_BASE_URL };
